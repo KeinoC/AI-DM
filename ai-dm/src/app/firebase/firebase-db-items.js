b6bc5d5b2f1cf7293701d5dd73d20ac96219082a
KeinoC <keino.chichester@gmail.com>
2023-08-19 11:44:06 -0400

    crud function for items db complete and ready for testing

--- ai-dm/src/app/firebase/firebase-db-items.js
+++ ai-dm/src/app/firebase/firebase-db-items.js
@@ -4,8 +4,10 @@ const {
     Timestamp,
     collection,
     getDocs,
+    getDoc,
     addDoc,
     updateDoc,
+    deleteDoc,
     getFirestore,
 } = require("firebase/firestore");
 import { auth, db } from "./firebase-config";
@@ -14,10 +16,9 @@ import { ITEMS } from "../utils/variables/database-vars";
 
 
 
-// * CRUD Methods * ----------------
+// * items collection CRUD Methods * ----------------
 
-
-
+// create item
 export async function createItem(ItemData) {
     try {
         const itemsCollection = collection(db, ITEMS)
@@ -32,6 +33,66 @@ export async function createItem(ItemData) {
         return newItemRef.id;
     } catch(error) {
         console.error('Error creating item: ', error);
+        throw error;
+    }
+}
+
+// get All items from db
+export async function getAllItems() {
+    try {
+        const itemsCollection = collection(db, ITEMS);
+        const querySnapshot = await getDocs(itemsCollection);
+
+        const allItems = [];
+        querySnapshot.forEach((item) => {
+            allItems.push({id: doc.id, ...doc.data()});
+        });
+
+        return allItems;
+    } catch (error) {
+        console.error('Error getting items: ', error);
+        throw error;
+    }
+}
+
+// get item by id
+export async function getItemById(itemId) {
+    try {
+        const itemDocRef = doc(db, ITEMS, itemId);
+        const itemSnapshot = await getDoc(itemDocRef);
+
+        if (itemSnapshot.exists()) {
+            const itemData =  itemSnapshot.data();
+            return { id: itemSnapshot.id, ...itemData };
+        }
+    } catch (error) {
+        console.error('Error getting item by ID: ', error);
+        throw error
+    }
+}
+
+// Update Item by ID
+export async function updateItem(itemId, updateData) {
+    try {
+        const itemDocRef = doc(db, ITEMS, itemId);
+        await updateDoc(itemDocRef, updateData);
+
+        console.log("Item updated successfully");
+    } catch (error) {
+        console.error('Error updating item: ', error);
+        throw error;
+    }
+}
+
+// Delete Item by ID
+export async function deleteItem(itemId) {
+    try {
+        const itemDocRef = doc(db, ITEMS, itemId);
+        await deleteDoc(itemDocRef);
+
+        console.log("Item deleted successfully");
+    } catch (error) {
+        console.error('Error deleting item: ', error);
         throw error;
     }
 }
--- ai-dm/src/app/page.jsx
+++ ai-dm/src/app/page.jsx
@@ -79,9 +79,8 @@ export default function Home() {
         <layout>
             <div>
                 {/* <div className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1 className="text-4xl font-bold text-center"> Welcome to our AI D&D Project</h1>
-    </div> */}
-
+                <h1 className="text-4xl font-bold text-center"> Welcome to our AI D&D Project</h1>
+                </div> */}
                 <AnimatedLogo />
             </div>
         </layout>
