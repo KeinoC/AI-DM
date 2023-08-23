07c97b80556d3143a67d959ffab0044ff55a943e
KeinoC <keino.chichester@gmail.com>
2023-08-23 15:44:26 -0400

    converting getAllItems to getItemsBy Categories

--- ai-dm/src/app/routes/grimoire/Tabs/ItemsTab.jsx
+++ ai-dm/src/app/routes/grimoire/Tabs/ItemsTab.jsx
@@ -1,22 +1,63 @@
 "use client";
 
 import { useDNDApi } from "@/app/contexts/DNDApiContext";
+import { getEquipmentListByCategory } from "@/app/dnd-api/api-equipment";
 import React, { useEffect, useState } from "react";
 
-export default function ItemsTab() {
-    const { apiItems } = useDNDApi();
+export default function EquipmentTab() {
+    const {
+        apiEquipment,
+        equipCatObjs,
+        selectedCat,
+        setSelectedCat,
+        selectedArray,
+        setSelectedArray,
+    } = useDNDApi();
 
-    console.log(apiItems)
+    // *** Equipment Category Workflow ***
 
+    // 1. User selects a category from the dropdown
+    const EquipCatFilterOptions = () => (
+        <select
+            value={selectedCat}
+            onChange={(e) => setSelectedCat(e.target.value)}
+        >
+            {equipCatObjs.map((cat) => (
+                <option key={cat.index} value={cat.index}>
+                    {cat.name}
+                </option>
+            ))}
+        </select>
+    );
 
-// *** Styles ***
+    // 2. User selects a category from the dropdown
+    useEffect(() => {
+        if (selectedCat) {
+            async function fetchEquipmentList() {
+                try {
+                    const categoryArray = await getEquipmentListByCategory(
+                        selectedCat
+                    );
+                    console.log(categoryArray); // Corrected the variable name from 'selectedArray' to 'categoryArray'
+                    setSelectedArray(categoryArray);
+                } catch (error) {
+                    console.error("Error fetching equipment list: ", error);
+                }
+            }
 
+            fetchEquipmentList();
+        }
+    }, [selectedCat]);
 
+    // ****** Render Cards by passing in array of equipment **********
 
-    function CategoryCards() {
-        return (apiItems || []).map((category, index) => {
+    function RenderEquipmentCards(equipmentArray) {
+        return (equipmentArray || []).map((category, index) => {
             return (
-                <div key={index} className="border-2 z-0 border-transparent card w-1/5 min-h-40% bg-base-100 shadow-2xl shadow-xl bg-opacity-50 overflow-hidden transform hover:border-yellow-500 hover:bg-opacity-90 hover:scale-105 ease-in-out duration-300 ">
+                <div
+                    key={index}
+                    className="border-2 z-0 border-transparent card w-1/5 min-h-40% bg-base-100 shadow-2xl shadow-xl bg-opacity-50 overflow-hidden transform hover:border-yellow-500 hover:bg-opacity-90 hover:scale-105 ease-in-out duration-300 "
+                >
                     <figure>
                         <img
                             src="https://www.tribality.com/wp-content/uploads/2021/04/elle-shengxuan-shi-0061.jpg"
@@ -25,11 +66,14 @@ export default function ItemsTab() {
                     </figure>
                     <div className="card-body">
                         <h2 className="card-title">{category.name}</h2>
-                        <p className="text-xs text-slate-400 overflow-y-scroll h-20">{category.desc || ""}</p>
-
+                        <p className="text-xs text-slate-400 overflow-y-scroll h-20">
+                            {category.desc || ""}
+                        </p>
 
                         <div className={"card-actions justify-end"}>
-                            <button className="btn hover:bg-yellow-500 hover:text-slate-500">Click me Zaddy</button>
+                            <button className="btn hover:bg-yellow-500 hover:text-slate-500">
+                                Click me Zaddy
+                            </button>
                         </div>
                     </div>
                 </div>
@@ -37,10 +81,43 @@ export default function ItemsTab() {
         });
     }
 
+    function CategoryCards() {
+        return (apiEquipment || []).map((category, index) => {
+            return (
+                <div
+                    key={index}
+                    className="border-2 z-0 border-transparent card w-1/5 min-h-40% bg-base-100 shadow-2xl shadow-xl bg-opacity-50 overflow-hidden transform hover:border-yellow-500 hover:bg-opacity-90 hover:scale-105 ease-in-out duration-300 "
+                >
+                    <figure>
+                        <img
+                            src="https://www.tribality.com/wp-content/uploads/2021/04/elle-shengxuan-shi-0061.jpg"
+                            alt={category.name}
+                        />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title">{category.name}</h2>
+                        <p className="text-xs text-slate-400 overflow-y-scroll h-20">
+                            {category.desc || ""}
+                        </p>
+
+                        <div className={"card-actions justify-end"}>
+                            <button className="btn hover:bg-yellow-500 hover:text-slate-500">
+                                Click me Zaddy
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            );
+        });
+    }
+
+    function CategoryCardsDisplay() {}
 
     return (
-    <div className= 'relative pt-40 flex flex-wrap justify-center gap-6 z-[-1]'>
-    <CategoryCards />
-    </div>
-    )
+        <div className="relative pt-40 flex flex-wrap justify-center gap-6 z-[-1]">
+            <EquipCatFilterOptions />
+            {/* <RenderEquipmentCards equipmentArray={getEquipmentListByCategory(selectedCat)} /> */}
+            {/* <CategoryCards /> */}
+        </div>
+    );
 }
