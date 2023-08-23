dc5a5f3104fb0890f6b24f2e8990b3fc91f3d70f
KeinoC <keino.chichester@gmail.com>
2023-08-22 22:29:22 -0400

    added functions to full 944 items from 5e api and set them to global state

--- ai-dm/src/app/contexts/DNDApiContext.js
+++ ai-dm/src/app/contexts/DNDApiContext.js
@@ -1,6 +1,9 @@
-'use client'
+"use client";
 import React, { createContext, useContext, useState, useEffect } from "react";
-import { getApiEquipmentByIndex } from "../dnd-api/api-equipment";
+import {
+    getApiEquipmentByIndex,
+    getEquipmentCategories,
+} from "../dnd-api/api-equipment";
 
 // * Initialize Context
 const DNDApiContext = createContext();
@@ -13,18 +16,106 @@ export const useDNDApi = () => {
 };
 
 export const DNDApiProvider = ({ children }) => {
+    const [apiItems, setAPIItems] = useState(0);
+    const [selectedCat, setSelectedCat] = useState("");
+    const [equipCatIndexes, setEquipCatIndexes] = useState([]);
+    const [equipCatObjs, setEquipCatObjs] = useState([]);
+    const [equipCatNames, setEquipCatNames] = useState([]);
 
-    const [apiItems, setAPIItems] = useState(0)
+    // equipment categories >> use categories url to get all items
 
+    // ******** Categories Data **********
+
+    // Get all equipment
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                // 1. Fetch the categories objects
+                const fetchedCategories = await getEquipmentCategories();
+                setEquipCatObjs(fetchedCategories?.results);
+    
+                // 2. Use the categories obj to get the urls
+                const catUrls = fetchedCategories.results.map(cat => cat?.url);
+    
+                // 3. Fetch data for each URL 
+                const intermediateResults = await Promise.all(
+                    catUrls.map(url => fetch(`https://www.dnd5eapi.co${url}`).then(res => res.json()))
+                );
+    
+                // Extract nested URLs from the fetched data
+                const nestedUrls = intermediateResults.flatMap(result => result.url);
+    
+                // 4. Fetch the nested data using the nested URLs
+                const finalResults = await Promise.all(
+                    nestedUrls.map(url => fetch(`https://www.dnd5eapi.co${url}`).then(res => res.json()))
+                );
+    
+                // Extract equipment data from the final results
+                const fetchedItems = finalResults.flatMap(result => result.equipment);
+    
+                // Set the items to global state
+                setAPIItems(fetchedItems);
+    
+            } catch (error) {
+                console.error("Error fetching categories: ", error);
+            }
+        };
+    
+        // Call the function
+        fetchCategories();
+    }, []);
+    
+    
+    console.log(apiItems)
+
+
+    // almost working code, trying different approach - delete if not needed
     // useEffect(() => {
-    //     const item = getApiEquipmentByIndex("shortsword")
-    // }, [])
+    //     const fetchCategories = async () => {
+    //         const catUrls = [];
+    //         const fetchedItems = [];
+
+    //         try {
+    //             // fetch the categories objects
+    //             const fetchedCategories = await getEquipmentCategories();
+    //             setEquipCatObjs(fetchedCategories?.results); // Set fetched items to allItems state
+
+    //             // use the categories obj just to get the urls
+    //             await fetchedCategories.results.map((cat) => {
+    //                 const url = cat?.url;
+    //                 catUrls.push(url);
+    //             });
+    //             console.log(catUrls);
+
+    //             // use the urls to get all items
+    //             catUrls.map(async (url) => {
+    //                 const res = await fetch(`https://www.dnd5eapi.co${url}`);
+    //                 const data = await res.json();
+    //                 data?.equipment?.map((item) => {
+    //                     fetchedItems.push(item);
+    //                 })
+    //                 // set the items to global state
+    //                 setAPIItems(fetchedItems);
+    //                 console.log("api items: "+apiItems)
+    //                 console.log("Item's state set successfully");
+    //             });
+    //         } catch (error) {
+    //             console.error("Error fetching categories: ", error);
+    //         }
+    //     };
+
+
+    //     // call the function
+    //     fetchCategories();
+    // }, []);
 
     const value = {
-        apiItems
+        apiItems,
     };
 
     return (
-        <DNDApiContext.Provider value={value}>{children}</DNDApiContext.Provider>
+        <DNDApiContext.Provider value={value}>
+            {children}
+        </DNDApiContext.Provider>
     );
-}
+};
--- ai-dm/src/app/dnd-api/api-equipment.js
+++ ai-dm/src/app/dnd-api/api-equipment.js
@@ -3,42 +3,18 @@
 export async function getApiEquipmentByIndex(index) {
     const res = await fetch(`https://www.dnd5eapi.co/api/equipment/${index}`);
     const data = await res.json();
-    console.log(data);
     return data;
 }
 
-/**
- * Retrieves all equipment items from the D&D 5e API.
- * @returns {Promise<Array>} - An array of equipment items.
- */
-// export async function getAllApiEquipment() {
-//     try {
-//         const allEquipment = [];
-//         let page = 1;
 
-//         while (true) {
-//             const res = await fetch(`https://www.dnd5eapi.co/api/equipment?page=${page}`);
-//             const data = await res.json();
+// ********** Equipment Categories **********
+export async function getEquipmentCategories() {
+    const res = await fetch(`https://www.dnd5eapi.co/api/equipment-categories`);
+    const data = await res.json();
+    return data;
+}
 
-//             if (data.results && data.results.length > 0) {
-//                 allEquipment.push(...data.results);
-//             } else {
-//                 break;
-//             }
 
-//             page++;
-//         }
-
-//         // Log the retrieved data (for debugging purposes)
-//         console.log(allEquipment);
-
-//         return allEquipment;
-//     } catch (error) {
-//         // Handle any errors that occur during the request or JSON conversion
-//         console.error('Error fetching equipment data:', error);
-//         throw error;
-//     }
-// }
 
 
 
