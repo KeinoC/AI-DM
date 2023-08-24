475219554d03d11122e34cc34e5b8fe7a4dae665
KeinoC <keino.chichester@gmail.com>
2023-08-23 23:16:45 -0400

    adventure route tweak

---
+++ ai-dm/src/app/contexts/AdventureContext.js
@@ -0,0 +1,28 @@
+'use client'
+import React, { createContext, useContext, useState } from "react";
+
+// * Initialize Context
+const AdventureContext = createContext();
+export const useAdventure = () => {
+    const context = useContext(AdventureContext);
+    if (context === undefined) {
+        throw new Error("ussAdventure must be used within a AdventureProvider");
+    }
+    return context;
+};
+
+export const AdventureProvider = ({ children }) => {
+
+    const [newAdventureMessage, setNewAdventureMessage] = useState([])
+
+    const value = {
+        adventureHistory,
+        setAdventureHistory,
+        newAdventureMessage,
+        setNewAdventureMessage,
+    };
+
+    return (
+        <AdventureContext.Provider value={value}>{children}</AdventureContext.Provider>
+    );
+}
