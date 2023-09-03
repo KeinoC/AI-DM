e5b6be36fc980e97b9723cada52336f2887819ae
Andrew Cook <andrewjcook93@gmail.com>
2023-09-03 10:04:05 -0500

    notes: my adventures

--- ai-dm/src/app/adventures/my/page.jsx
+++ ai-dm/src/app/adventures/my/page.jsx
@@ -2,6 +2,8 @@ export default function MyAdventures() {
   return (
     <div>
       My Games Go Here
+
+      {/* List of Games attached to user. review other websites for UX / UI inspiration, but do it better than them! */}
     </div>
   )
 }
