cd0686230a18f5557b7c886a93634eba7df9847d
KeinoC <keino.chichester@gmail.com>
2023-10-10 20:30:34 -0400

    clean up adventure

--- ai-dm/src/app/adventures/page.jsx
+++ ai-dm/src/app/adventures/page.jsx
@@ -1,7 +1,6 @@
 'use client'
 import React from 'react';
 import AllAdventures from './all-adventures/AllAdventures';
-import TopBar from './all-adventures/TopBar';
 
 export default function AdventureRoute() {
     return (
