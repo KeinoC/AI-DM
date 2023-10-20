378fd31ef17eb70ee3ac934a606de311aa978a2d
KeinoC <keino.chichester@gmail.com>
2023-10-19 22:59:48 -0400

    more dependency work

--- ai-dm/src/app/routes/login/page.jsx
+++ ai-dm/src/app/routes/login/page.jsx
@@ -3,7 +3,6 @@
 import React from "react";
 import Login from "./Login";
 import { UserProvider } from "../../contexts/UserContext";
-import { AnimatePresence, motion } from "framer-motion";
 
 export default function LoginPage() {
     return (
